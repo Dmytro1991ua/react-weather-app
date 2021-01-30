e0cac06e0d740b81cd7473c3dfc2a86139c551c6
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2021-01-30 08:51:16 +0200

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Weather App</h1>
+      </main>
     </div>
   );
 }
